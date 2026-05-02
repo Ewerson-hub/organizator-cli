d8cd749a7a1ae89bfd805c78de5371a4d4465e59
Ewerson-hub <ewersonl10@gmail.com>
2026-05-02 17:10:04 -0300

    feat: add a graceful shotdown for CTRL + C command

--- src/core.js
+++ src/core.js
@@ -1,6 +1,53 @@
 const fs = require('node:fs/promises')
 const path = require("node:path")
+const controller = new AbortController();
 
+async function organizationByType(src, dest, ext, recursive, signal) {
+    try {
+        const readdirOptions = (!recursive) ? { withFileTypes: true, signal: signal } : { withFileTypes: true, recursive: true, signal: signal };
+
+        const files = await fs.readdir(src, readdirOptions)
+
+        if (files.length === 0) console.log('Erro ! A pasta não contem arquivos para organizar')
+
+        //cache    
+        const createdDirs = new Set();
+        const extensionsWhiteList = new Set(ext);
+        
+        for (const file of files) {
+
+            const fileInitialSrc = path.join(file.path, file.name);
+
+            //pega extensao do arquivo sem o ponto
+            const extensionName = path.extname(fileInitialSrc).slice(1);
+
+            //checa se a extensão esta na lista de arquivos desejado
+            if (extensionsWhiteList.has(extensionName)) {
+
+                //checa se o diretorio ja existe, se não -> cria ele 
+                if (!createdDirs.has(extensionName)) {
+                    await fs.mkdir(path.join(src, extensionName), { recursive: true, signal: signal })
+                    createdDirs.add(extensionName)
+                }
+
+
+                const fileFinalDest = path.join(dest, extensionName, file.name)
+
+                if (fileInitialSrc != fileFinalDest) {
+                    await fs.rename(fileInitialSrc, fileFinalDest, { signal: signal }).then(() => {
+                        console.log(`Moved ${file.name} --> ${fileFinalDest}`)
+                    })
+                }
+
+            }
+
+        }
+
+    } catch (err) {
+        console.log('erro no try : ', err)
+    }
+
+}
 
 module.exports = {
     printHello: (array) => {
@@ -8,51 +55,13 @@ module.exports = {
         console.log(os.platform())
     },
 
-    startOrganization: async (src, dest, mode, ext, recursive) => { 
-        try{
-            const readdirOptions = (!recursive)? {withFileTypes: true} : {withFileTypes: true, recursive: true};
-            
-            const files = await fs.readdir(src, readdirOptions)
+    startOrganization: (src, dest, mode, ext, recursive) => {
 
-            if(files.length === 0) console.log('Erro ! A pasta não contem arquivos para organizar')
+        process.on('SIGINT', () => {
+            console.log('Cancelando escrita')
+            controller.abort();
+        })
 
-            const createdDirs = new Set();
-            const extensionsWhiteList = new Set(ext);
-            
-
-            for (const file of files) {
-                
-                const fileInitialSrc = path.join(file.path, file.name);
-
-                //pega extensao do arquivo sem o ponto
-                const extensionName = path.extname(fileInitialSrc).slice(1);
- 
-                //checa se a extensão esta na lista de arquivos desejado
-                if(extensionsWhiteList.has(extensionName)){
-
-                    //checa se o diretorio ja existe, se não -> cria ele 
-                    if(!createdDirs.has(extensionName)){
-                        await fs.mkdir(path.join(src, extensionName), {recursive:true})
-                        createdDirs.add(extensionName)
-                    }
-
-  
-                    const fileFinalDest = path.join(dest, extensionName,file.name)
-
-                    if( fileInitialSrc != fileFinalDest){
-                         await fs.rename(fileInitialSrc, fileFinalDest).then(() => {
-                            console.log(`Moved ${file.name} --> ${fileFinalDest}`)
-                        })
-                    }
-                              
-                }
-
-
-            }
-
-        }catch(err){
-            console.log('erro no try : ',err)
-        }
-
+        organizationByType(src, dest, ext, recursive, controller.signal)
     }
 }
