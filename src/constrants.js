38d3dd854b2784960889596c5fb6b54acb5cf015
Ewerson-hub <ewersonl10@gmail.com>
2026-06-14 23:26:04 -0300

    refactor: add new organization mode

--- src/constrants.js
+++ src/constrants.js
@@ -1,6 +1,10 @@
 const MODES = {
     TYPE: 'type',
+    //yaer and month
+    DATE: 'date',
+    //only month
     DATE_MONTH :'month',
+    //only year
     DATE_YEAR :'year',
     TYPE_DATE: 'type-date'
 }
