3bf5548e5b0808f6e3e03f319b912abfb807dd8d
Your git config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2026-09-07 18:25:52 -0300

    feat: test flag added

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 const {program, Option} = require('commander')
 const path = require('node:path')
-const {printHello, startOrganization} = require("../src/core")
+const {resetFolders, startOrganization} = require("../src/core")
 const {MODES, EXTENSIONS} = require("../src/constrants")
 
 
@@ -10,27 +10,36 @@ program.name("organizator")
 .requiredOption("-s, --source <paths ...>", "informe o local de onde estão os arquivos")
 .option("-d, --destination [path]", "informe o local onde os arquivos devem ficar")
 .option("-r, --recursive", "Define se o organizador deve procurar e organizar arquivos contidos em subpastas.")
+.option("-t, --test", "comando para fins de testes de desenvolvimento")
+
 .addOption(new Option('-m, --mode <type>', "modos de organização")
 .choices(Object.values(MODES)).default(MODES.TYPE))
+
 .addOption(new Option('-e, --extensions <extensions...>', "filtro de extensoẽs")
 .choices(Object.values(EXTENSIONS)).default(EXTENSIONS))
+
 .action((options) => {
     
-    
+    //adicionar validacoes para casos de nao informacao do src
     const src = (options.source == '.')? process.cwd() : path.resolve(options.source);
     
-    const dest = (options.destination)? path.resolve(options.dest) : src;
+    const dest = (options.destination)? path.resolve(options.destination) : src;
     
     const mode = options.mode;
     
     const ext = options.extensions;
     
-    const recursive = options.recursive;
-    
+    const recursive = options.recursive ?? false;
 
+    const test = options.test ?? false
     
-    // printHello([src, dest, mode, ext])
-    startOrganization(src, dest, mode, ext, recursive)
+    
+    if(!test){
+        startOrganization(src, dest, mode, ext, recursive)
+    }else{
+        resetFolders(src)
+    }
+    
     
 });
 program.parse();
