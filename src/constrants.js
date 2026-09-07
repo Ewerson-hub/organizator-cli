c2e53ec0238660b5becbb9807149c84e5c8ef849
Your git config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2026-09-07 18:26:42 -0300

    refactor: unnecessary comments removed

--- src/constrants.js
+++ src/constrants.js
@@ -1,10 +1,7 @@
 const MODES = {
     TYPE: 'type',
-    //yaer and month
     DATE: 'date',
-    //only month
     DATE_MONTH :'month',
-    //only year
     DATE_YEAR :'year',
     TYPE_DATE: 'type-date'
 }
