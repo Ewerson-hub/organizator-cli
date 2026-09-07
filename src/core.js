9d431d3712cff471674a534ce71fdfee506589e3
Your git config --global user.email you@example.com  git config --global user.name Your <you@example.com>
2026-09-07 18:27:12 -0300

    feat: test function implemented

--- src/core.js
+++ src/core.js
@@ -1,38 +1,61 @@
-const {organizationController} = require('./controller.js')
+const { organizationController } = require('./controller.js')
 const fs = require('node:fs/promises')
+const path = require("node:path")
 const controller = new AbortController();
 const chalk = require('chalk');
 
 module.exports = {
-    printHello: (array) => {
-        //função simples para testar qualquer coisa que eu quiser.
-        console.log('os.platform()')
+    resetFolders: async (src) => {
+        const files = await fs.readdir(src, { withFileTypes: true, recursive: true })
+
+        if (!files.length) {
+            console.log(chalk.yellow('A pasta não contem arquivos para organizar'))
+            return
+        }
+
+        const emptyFolders = new Set();
+
+        for (const file of files) {
+            if (file.isFile()) {
+                emptyFolders.add(file.parentPath)
+                await fs.rename(path.join(file.parentPath, file.name), path.join(src, file.name))
+                console.log('\n' + chalk.bold(`${file.name}`) + chalk.dim(' moved to ') + chalk.green(`${path.dirname(path.join(file.parentPath, file.name))}`))
+            }
+
+        }
+
+        emptyFolders.forEach(path => {
+            fs.rmdir(path)
+        })
+
+        console.log(emptyFolders)
+
     },
 
     startOrganization: async (src, dest, mode, ext, recursive) => {
 
         process.on('SIGINT', () => {
-            console.log('\n'+chalk.red('Operação cancelada !'))
+            console.log('\n' + chalk.red('Operação cancelada !'))
             controller.abort();
         })
 
         try {
             const signal = controller.signal;
             const readdirOptions = (!recursive) ? { withFileTypes: true, signal: signal } : { withFileTypes: true, recursive: true, signal: signal };
-            
+
             const files = await fs.readdir(src, readdirOptions)
-        
-            if (!files.length){
+
+            if (!files.length) {
                 console.log(chalk.yellow('A pasta não contem arquivos para organizar'))
                 return
-            } 
-            
+            }
+
             //cache    
             const createdDirs = new Set();
             const extensionsWhiteList = new Set(ext);
 
 
-            organizationController(mode, {src: src, dest: dest, signal:signal, files:files, createdDirs: createdDirs, extensionsWhiteList: extensionsWhiteList})
+            organizationController(mode, { src: src, dest: dest, signal: signal, files: files, createdDirs: createdDirs, extensionsWhiteList: extensionsWhiteList })
 
         } catch (err) {
             console.log(chalk.red(`Erro na execução -> ${err.message}`))
